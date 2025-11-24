72fbfee3d1ab788bd33b127ff886633940fee484
Hector Uribe <dgic.h_uribe@gobjuarez.mpio>
2025-11-24 12:57:42 -0700

    Se agrega dominio de intranet

--- rescate/settings.py
+++ rescate/settings.py
@@ -26,11 +26,12 @@ SECRET_KEY = 'django-insecure-jwq=u@wby$5@yfq4-_5tj@0&h#3yga=h-87&gvn+j%s-s4^&^%
 DEBUG = True
 BASE_DD_PRUEBA = True
 
-ALLOWED_HOSTS = ['rescate.juarez.gob.mx', 'localhost', '127.0.0.1', '10.236.62.93']
+ALLOWED_HOSTS = ['rescate.juarez.gob.mx', 'rescate.gobjuarez.mpio', 'localhost', '127.0.0.1', '10.236.62.93']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://rescate.juarez.gob.mx',
     'https://10.236.62.93',
+    'https://rescate.gobjuarez.mpio'
 ]
 
 # Application definition
